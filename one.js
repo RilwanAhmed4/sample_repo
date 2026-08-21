8644ed442bd27e6e866b056d59d52c102bcd40e1
RilwanAhmed4 <rilwanahmed4@gmail.com>
2026-08-21 20:39:25 +0530

    update a file

--- one.js
+++ one.js
@@ -1,3 +1,5 @@
-console.log("jhello hii");
+console.log("hello hii");
+console.log("git helps to syncronize the code");
+console.log("github is an online platform");
 console.log("git is a version control software");
 console.log("git helps helps to syncronize the code");
